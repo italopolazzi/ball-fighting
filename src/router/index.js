d527ac703b2f2ddeb4eeff1bb4c036b0aa77dc7f
Ítalo Polazzi Ferreira <itpzzi@gmail.com>
2020-01-20 04:16:58 -0300

    add: routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,115 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Launcher from "@/views/Launcher.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+const routes = [{
+        path: "/",
+        component: Launcher,
+        children: [{
+                name: "launcher-hall",
+                path: '',
+                component: () =>
+                    import ( /* webpackChunkName: "hall" */ "@/views/Launcher/Hall")
+            },
+            {
+                name: "launcher-play",
+                path: 'launcher-play',
+                component: () =>
+                    import ( /* webpackChunkName: "play" */ "@/views/Launcher/Play")
+            },
+            {
+                name: "launcher-about",
+                path: 'launcher-about',
+                component: () =>
+                    import ( /* webpackChunkName: "about" */ "@/views/Launcher/About")
+            },
+            {
+                name: "launcher-options",
+                path: 'launcher-options',
+                component: () =>
+                    import ( /* webpackChunkName: "options" */ "@/views/Launcher/Options"),
+                children: [{
+                        name: 'options-controls',
+                        path: '/options-controls',
+                        component: () =>
+                            import ( /* webpackChunkName: "options-controls" */ "@/views/Launcher/Options/Controls.vue")
+                    },
+                    {
+                        name: 'options-display',
+                        path: '/options-display',
+                        component: () =>
+                            import ( /* webpackChunkName: "options-display" */ "@/views/Launcher/Options/Display.vue")
+                    },
+                    {
+                        name: 'options-language',
+                        path: '/options-language',
+                        component: () =>
+                            import ( /* webpackChunkName: "options-language" */ "@/views/Launcher/Options/Language.vue")
+                    },
+                    {
+                        name: 'options-profile',
+                        path: '/options-profile',
+                        component: () =>
+                            import ( /* webpackChunkName: "options-profile" */ "@/views/Launcher/Options/Profile.vue")
+                    },
+                ]
+            }
+        ]
+    },
+    {
+        path: "/game",
+        component: () =>
+            import ( /* webpackChunkName: "game" */ "@/views/Game.vue"),
+        meta: { protected: true },
+        children: [{
+                name: 'game-online-remote',
+                path: 'game-online-remote',
+                component: () =>
+                    import ( /* webpackChunkName: "game-online-remote" */ "@/views/Game/OnlineRemote.vue")
+            },
+            {
+                name: 'game-online-local',
+                path: 'game-online-local',
+                component: () =>
+                    import ( /* webpackChunkName: "game-online-local" */ "@/views/Game/OnlineLocal.vue")
+            },
+            {
+                name: 'game-offline-single',
+                path: 'game-offline-single',
+                component: () =>
+                    import ( /* webpackChunkName: "game-offline-single" */ "@/views/Game/OfflineSingle.vue")
+            },
+            {
+                name: 'game-offline-dual',
+                path: 'game-offline-dual',
+                component: () =>
+                    import ( /* webpackChunkName: "game-offline-dual" */ "@/views/Game/OfflineDual.vue")
+            },
+
+        ]
+    }
+];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+    routes,
+    base: process.env.BASE_URL,
+    routes
+});
 
-export default router
+// router.beforeEach((to, from, next) => {
+//     if (to.matched.some(record => record.meta.protected)) {
+//         if (to.name.startsWith("game")) {
+//             if (to.params.start) {
+//                 next()
+//             } else {
+//                 next(false)
+//             }
+//         }
+//     } else {
+//         next()
+//     }
+// })
+
+export default router;
