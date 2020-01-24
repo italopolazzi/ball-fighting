45bcfe730eff00a006287d63d5d7d063fe4cd065
Ítalo Polazzi Ferreira <itpzzi@gmail.com>
2020-01-24 02:37:28 -0300

    add: basic structure for a register

--- src/store/modules/register/dual.js
+++ src/store/modules/register/dual.js
@@ -1,9 +1,39 @@
+import AgentPlayer from "@/game/scripts/Players/AgentPlayer"
+import HumanPlayer from "@/game/scripts/Players/HumanPlayer"
+
 export default {
     namespaced: true,
-    state: {},
+    state: {
+        players: {
+            client: {
+                type: HumanPlayer,
+                nickname: null,
+                controls: null,
+                character: null
+            },
+            guest: {
+                type: AgentPlayer,
+                nickname: null,
+                controls: null,
+                character: null
+            }
+        },
+        characters: null,
+        genomes: null
+    },
     mutations: {},
     actions: {},
     getters: {
-
+        getPlayersByType: state => class_type => {
+            const players = Object.values(state.players)
+            const filtered = players.filter(p => p.type === class_type)
+            return filtered
+        },
+        getHumanPlayers: (state, getters) => {
+            return getters.getPlayersByType(HumanPlayer)
+        },
+        getAgentPlayers: (state, getters) => {
+            return getters.getPlayersByType(AgentPlayer)
+        }
     }
 }
--- src/views/Launcher/Play/RegisterOfflineDual.vue
+++ src/views/Launcher/Play/RegisterOfflineDual.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="register-offline-dual">
-   
+    
   </div>
 </template>
 
